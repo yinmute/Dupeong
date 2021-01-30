b4b091a6bb99db24b354df9e671c2d2651c4e823
Artem Nesiolovskiy <yinmute@gmail.com>
2021-01-30 12:46:39 -0800

    Initial Commit

---
+++ Dupeong/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Dupeong
+//
+//  Created by Nesiolovskiy, Artem on 1/30/21.
+//
+
+#include <iostream>
+#include <SDL2/SDL.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
