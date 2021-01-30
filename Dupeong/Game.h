6acf940a81e3495d823662ea04bebdd74d33899f
Artem Nesiolovskiy <yinmute@gmail.com>
2021-01-30 13:15:24 -0800

    Adding initial Game.h header file with Game class

---
+++ Dupeong/Game.h
@@ -0,0 +1,41 @@
+//
+//  Game.h
+//  Dupeong
+//
+//  Created by Nesiolovskiy, Artem on 1/30/21.
+//
+
+#pragma once
+#include "SDL/SDL.h"
+
+class Game {
+    
+public:
+    
+    Game();
+    
+    // Initialize the game
+    bool Initialize();
+    
+    // Runs the game loop until the game is over
+    void RunLoop();
+    
+    // Shut down the game
+    void Shutdown();
+    
+private:
+    
+    // Helper functions for the game loop
+    
+    void ProcessInput();
+    void UpdateGame();
+    void GenerateOutput();
+    
+    // Window created by SDL
+    SDL_Windows* mWindow;
+    
+    // Game should continue to run
+    bool mIsRunning;
+    
+    
+}
